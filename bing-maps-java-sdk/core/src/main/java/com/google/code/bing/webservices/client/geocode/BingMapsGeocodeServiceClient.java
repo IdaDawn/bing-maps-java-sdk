b8334d6d7e3e6814fa039387ac8ccbce828783fe
nabeelmukhtar <nabeelmukhtar@75f37d7e-2936-11df-9785-5baaa8915590>
2010-03-08 15:17:28 +0000

    Initial commit of bing-maps-java-sdk

--- bing-maps-java-sdk/core/src/main/java/com/google/code/bing/webservices/client/geocode/BingMapsGeocodeServiceClient.java
+++ bing-maps-java-sdk/core/src/main/java/com/google/code/bing/webservices/client/geocode/BingMapsGeocodeServiceClient.java
@@ -3,10 +3,12 @@
  */
 package com.google.code.bing.webservices.client.geocode;
 
+import com.google.code.bing.webservices.client.BingMapsWebServicesClient;
+
 /**
  * The Interface BingMapsWebServiesClient. 
  * 
  * @author Nabeel Mukhtar
  */
-public interface BingMapsGeocodeServiceClient {
+public interface BingMapsGeocodeServiceClient extends BingMapsWebServicesClient {
 }
--- bing-maps-java-sdk/core/src/main/java/com/google/code/bing/webservices/client/route/BingMapsRouteServiceClient.java
+++ bing-maps-java-sdk/core/src/main/java/com/google/code/bing/webservices/client/route/BingMapsRouteServiceClient.java
@@ -3,10 +3,12 @@
  */
 package com.google.code.bing.webservices.client.route;
 
+import com.google.code.bing.webservices.client.BingMapsWebServicesClient;
+
 /**
  * The Interface BingMapsWebServiesClient. 
  * 
  * @author Nabeel Mukhtar
  */
-public interface BingMapsRouteServiceClient {
+public interface BingMapsRouteServiceClient extends BingMapsWebServicesClient {
 }
