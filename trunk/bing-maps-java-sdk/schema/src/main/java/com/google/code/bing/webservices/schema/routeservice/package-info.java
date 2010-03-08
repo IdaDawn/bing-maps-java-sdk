ede44d99d6cb506ba4b32bb1e37ca6750180c6bf
nabeelmukhtar <nabeelmukhtar@75f37d7e-2936-11df-9785-5baaa8915590>
2010-03-08 13:48:50 +0000

    reverted to previous version.

--- trunk/bing-maps-java-sdk/schema/src/main/java/com/google/code/bing/webservices/schema/routeservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://dev.virtualearth.net/webservices/v1/route", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.google.code.bing.webservices.schema.routeservice;
