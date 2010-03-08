e0e30f87a928f5f5dff9a19d995a8219bccb5daf
nabeelmukhtar <nabeelmukhtar@75f37d7e-2936-11df-9785-5baaa8915590>
2010-03-08 13:48:38 +0000

    reverted to previous version.

--- bing-maps-java-sdk/schema/src/main/java/com/google/code/bing/webservices/schema/imageryservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://dev.virtualearth.net/webservices/v1/common", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.google.code.bing.webservices.schema.imageryservice;
