a86f9f320110d1bb5274111f75b0b739d2029d18
nabeelmukhtar <nabeelmukhtar@75f37d7e-2936-11df-9785-5baaa8915590>
2010-03-08 15:15:45 +0000

    Initial commit of bing-maps-java-sdk

---
+++ trunk/bing-maps-java-sdk/core/src/main/java/com/google/code/bing/webservices/client/BingMapsWebServicesClient.java
@@ -0,0 +1,12 @@
+/**
+ *
+ */
+package com.google.code.bing.webservices.client;
+
+/**
+ * The Interface BingMapsWebServiesClient. 
+ * 
+ * @author Nabeel Mukhtar
+ */
+public interface BingMapsWebServicesClient {
+}
