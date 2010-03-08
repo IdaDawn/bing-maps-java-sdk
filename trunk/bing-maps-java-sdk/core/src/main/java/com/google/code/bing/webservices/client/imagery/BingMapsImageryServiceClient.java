f650a4bc9141e1f0950a81810dddee9472acbb58
nabeelmukhtar <nabeelmukhtar@75f37d7e-2936-11df-9785-5baaa8915590>
2010-03-08 15:17:28 +0000

    Initial commit of bing-maps-java-sdk

--- trunk/bing-maps-java-sdk/core/src/main/java/com/google/code/bing/webservices/client/imagery/BingMapsImageryServiceClient.java
+++ trunk/bing-maps-java-sdk/core/src/main/java/com/google/code/bing/webservices/client/imagery/BingMapsImageryServiceClient.java
@@ -3,10 +3,12 @@
  */
 package com.google.code.bing.webservices.client.imagery;
 
+import com.google.code.bing.webservices.client.BingMapsWebServicesClient;
+
 /**
  * The Interface BingMapsWebServiesClient. 
  * 
  * @author Nabeel Mukhtar
  */
-public interface BingMapsImageryServiceClient {
+public interface BingMapsImageryServiceClient extends BingMapsWebServicesClient {
 }
--- trunk/bing-maps-java-sdk/core/src/main/java/com/google/code/bing/webservices/client/search/BingMapsSearchServiceClient.java
+++ trunk/bing-maps-java-sdk/core/src/main/java/com/google/code/bing/webservices/client/search/BingMapsSearchServiceClient.java
@@ -3,10 +3,12 @@
  */
 package com.google.code.bing.webservices.client.search;
 
+import com.google.code.bing.webservices.client.BingMapsWebServicesClient;
+
 /**
  * The Interface BingMapsWebServiesClient. 
  * 
  * @author Nabeel Mukhtar
  */
-public interface BingMapsSearchServiceClient {
+public interface BingMapsSearchServiceClient extends BingMapsWebServicesClient {
 }
