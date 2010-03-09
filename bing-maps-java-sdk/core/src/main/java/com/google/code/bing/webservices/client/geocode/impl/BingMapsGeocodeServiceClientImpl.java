10f1da9b53dc238fd0b565e9493adcdafc508635
nabeelmukhtar <nabeelmukhtar@75f37d7e-2936-11df-9785-5baaa8915590>
2010-03-09 13:11:41 +0000

    Initial commit of bing-maps-java-sdk

--- bing-maps-java-sdk/core/src/main/java/com/google/code/bing/webservices/client/geocode/impl/BingMapsGeocodeServiceClientImpl.java
+++ bing-maps-java-sdk/core/src/main/java/com/google/code/bing/webservices/client/geocode/impl/BingMapsGeocodeServiceClientImpl.java
@@ -2,12 +2,13 @@ package com.google.code.bing.webservices.client.geocode.impl;
 
 import javax.xml.ws.WebServiceRef;
 
-import net.virtualearth.dev.webservices.v1.geocode.IGeocodeService;
+import net.virtualearth.dev.webservices.v1.geocode.GeocodeService;
 
 import com.google.code.bing.webservices.client.geocode.BingMapsGeocodeServiceClient;
 
 public class BingMapsGeocodeServiceClientImpl implements
 		BingMapsGeocodeServiceClient {
+	
 	@WebServiceRef(wsdlLocation="http://dev.virtualearth.net/webservices/v1/metadata/geocodeservice/geocodeservice.wsdl")
-	static IGeocodeService geocodeService;
+	static GeocodeService geocodeService;
 }
