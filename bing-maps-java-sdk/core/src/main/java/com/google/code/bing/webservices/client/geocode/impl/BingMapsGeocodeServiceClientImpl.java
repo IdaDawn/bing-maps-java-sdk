23b133b5ed17d41f7d843fe5076aa39b82a382b0
nabeelmukhtar <nabeelmukhtar@75f37d7e-2936-11df-9785-5baaa8915590>
2010-03-09 15:28:57 +0000

    Stub client implementations

--- bing-maps-java-sdk/core/src/main/java/com/google/code/bing/webservices/client/geocode/impl/BingMapsGeocodeServiceClientImpl.java
+++ bing-maps-java-sdk/core/src/main/java/com/google/code/bing/webservices/client/geocode/impl/BingMapsGeocodeServiceClientImpl.java
@@ -3,12 +3,19 @@ package com.google.code.bing.webservices.client.geocode.impl;
 import javax.xml.ws.WebServiceRef;
 
 import net.virtualearth.dev.webservices.v1.geocode.GeocodeService;
+import net.virtualearth.dev.webservices.v1.geocode.IGeocodeService;
 
 import com.google.code.bing.webservices.client.geocode.BingMapsGeocodeServiceClient;
 
 public class BingMapsGeocodeServiceClientImpl implements
 		BingMapsGeocodeServiceClient {
 	
-	@WebServiceRef(wsdlLocation="http://dev.virtualearth.net/webservices/v1/metadata/geocodeservice/geocodeservice.wsdl")
+	@WebServiceRef(wsdlLocation="geocodeservice.wsdl")
 	static GeocodeService geocodeService;
+	
+	public static void main(String[] args) throws Exception {
+		geocodeService = new GeocodeService();
+		IGeocodeService proxy = geocodeService.getBasicHttpBindingIGeocodeService();
+		proxy.geocode(null);
+	}
 }
