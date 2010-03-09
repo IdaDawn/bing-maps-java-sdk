0dedf3c1af9e36fd432b824174729dc7c3e84d8c
nabeelmukhtar <nabeelmukhtar@75f37d7e-2936-11df-9785-5baaa8915590>
2010-03-09 13:11:41 +0000

    Initial commit of bing-maps-java-sdk

--- trunk/bing-maps-java-sdk/core/src/main/java/com/google/code/bing/webservices/client/imagery/impl/BingMapsImageryServiceClientImpl.java
+++ trunk/bing-maps-java-sdk/core/src/main/java/com/google/code/bing/webservices/client/imagery/impl/BingMapsImageryServiceClientImpl.java
@@ -2,7 +2,7 @@ package com.google.code.bing.webservices.client.imagery.impl;
 
 import javax.xml.ws.WebServiceRef;
 
-import net.virtualearth.dev.webservices.v1.imagery.IImageryService;
+import net.virtualearth.dev.webservices.v1.imagery.ImageryService;
 
 import com.google.code.bing.webservices.client.imagery.BingMapsImageryServiceClient;
 
@@ -10,5 +10,5 @@ public class BingMapsImageryServiceClientImpl implements
 		BingMapsImageryServiceClient {
 	
 	@WebServiceRef(wsdlLocation="http://dev.virtualearth.net/webservices/v1/metadata/imageryservice/imageryservice.wsdl")
-	static IImageryService imageryService;
+	static ImageryService imageryService;
 }
--- trunk/bing-maps-java-sdk/core/src/main/java/com/google/code/bing/webservices/client/route/impl/BingMapsRouteServiceClientImpl.java
+++ trunk/bing-maps-java-sdk/core/src/main/java/com/google/code/bing/webservices/client/route/impl/BingMapsRouteServiceClientImpl.java
@@ -5,7 +5,7 @@ package com.google.code.bing.webservices.client.route.impl;
 
 import javax.xml.ws.WebServiceRef;
 
-import net.virtualearth.dev.webservices.v1.route.IRouteService;
+import net.virtualearth.dev.webservices.v1.route.RouteService;
 
 import com.google.code.bing.webservices.client.route.BingMapsRouteServiceClient;
 
@@ -17,5 +17,5 @@ public class BingMapsRouteServiceClientImpl implements
 		BingMapsRouteServiceClient {
 
 	@WebServiceRef(wsdlLocation="http://dev.virtualearth.net/webservices/v1/metadata/routeservice/routeservice.wsdl")
-	static IRouteService routeService;
+	static RouteService routeService;
 }
