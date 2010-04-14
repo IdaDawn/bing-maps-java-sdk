f7570c48a8e2f1ee8dcf5cba6d9b37752ee1553c
nabeelmukhtar <nabeelmukhtar@75f37d7e-2936-11df-9785-5baaa8915590>
2010-04-14 16:51:47 +0000

    Request builders partial commit.

--- bing-maps-java-sdk/core/src/main/java/com/google/code/bing/webservices/client/geocode/BingMapsGeocodeServiceClient.java
+++ bing-maps-java-sdk/core/src/main/java/com/google/code/bing/webservices/client/geocode/BingMapsGeocodeServiceClient.java
@@ -3,6 +3,7 @@
  */
 package com.google.code.bing.webservices.client.geocode;
 
+import net.virtualearth.dev.webservices.v1.common.Confidence;
 import net.virtualearth.dev.webservices.v1.geocode.GeocodeRequest;
 import net.virtualearth.dev.webservices.v1.geocode.ReverseGeocodeRequest;
 
@@ -20,10 +21,22 @@ public interface BingMapsGeocodeServiceClient extends BingMapsWebServicesClient 
 	public ReverseGeocodeRequestBuilder newReverseGeocodeRequestBuilder();
 	
 	public interface GeocodeRequestBuilder extends RequestBuilder<GeocodeRequest> {
-		
+		public GeocodeRequestBuilder withQuery(String query);
+		public GeocodeRequestBuilder withAddressLine(String addressLine);
+		public GeocodeRequestBuilder withAdminDistrict(String adminDistrict);
+		public GeocodeRequestBuilder withCountryRegion(String countryRegion);
+		public GeocodeRequestBuilder withDistrict(String district);
+		public GeocodeRequestBuilder withFormattedAddress(String formattedAddress);
+		public GeocodeRequestBuilder withLocality(String locality);
+		public GeocodeRequestBuilder withPostalCode(String postalCode);
+		public GeocodeRequestBuilder withPostalTown(String postalTown);
+		public GeocodeRequestBuilder withCount(int count);
+		public GeocodeRequestBuilder withConfidence(Confidence confidence);
 	}
 	
 	public interface ReverseGeocodeRequestBuilder extends RequestBuilder<ReverseGeocodeRequest> {
-		
+		public ReverseGeocodeRequestBuilder withAltitude(double altitude);
+		public ReverseGeocodeRequestBuilder withLatitude(double latitude);
+		public ReverseGeocodeRequestBuilder withLongitude(double longitude);
 	}
 }
