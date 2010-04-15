e2d7bfd5326abb432623d86e86051dc6cfc94e8a
nabeelmukhtar <nabeelmukhtar@75f37d7e-2936-11df-9785-5baaa8915590>
2010-04-15 16:31:58 +0000

    Example applications.

--- bing-maps-java-sdk/examples/src/java/com/google/code/bing/webservices/example/BingMapsGeocodeServiceSample.java
+++ bing-maps-java-sdk/examples/src/java/com/google/code/bing/webservices/example/BingMapsGeocodeServiceSample.java
@@ -1,15 +1,13 @@
 package com.google.code.bing.webservices.example;
 
 import net.virtualearth.dev.webservices.v1.common.Confidence;
-import net.virtualearth.dev.webservices.v1.common.Credentials;
-import net.virtualearth.dev.webservices.v1.geocode.ArrayOfFilterBase;
-import net.virtualearth.dev.webservices.v1.geocode.ConfidenceFilter;
-import net.virtualearth.dev.webservices.v1.geocode.GeocodeOptions;
+import net.virtualearth.dev.webservices.v1.common.GeocodeResult;
 import net.virtualearth.dev.webservices.v1.geocode.GeocodeRequest;
 import net.virtualearth.dev.webservices.v1.geocode.GeocodeResponse;
 
 import com.google.code.bing.webservices.client.BingMapsWebServicesClientFactory;
 import com.google.code.bing.webservices.client.geocode.BingMapsGeocodeServiceClient;
+import com.google.code.bing.webservices.client.geocode.BingMapsGeocodeServiceClient.GeocodeRequestBuilder;
 
 public class BingMapsGeocodeServiceSample {
 	
@@ -21,27 +19,16 @@ public class BingMapsGeocodeServiceSample {
 	}
 	
 	private static void printResponse(GeocodeResponse response) {
-		// TODO Auto-generated method stub
-		
+		for (GeocodeResult result : response.getResults().getGeocodeResult()) {
+			System.out.println(result.getDisplayName());
+		}
 	}
 
 	private static GeocodeRequest createGeocodeRequest(BingMapsGeocodeServiceClient client) {
-		GeocodeRequest request = GEOCODE_FACTORY.createGeocodeRequest();
-		
-		Credentials credential = COMMON_FACTORY.createCredentials();
-		credential.setApplicationId("AgBXisHgZAEfpDnT95skGJiYu_Oh9XgeAi7O0UJfhg_GdEYB2yeeETJ8ayQ-3kNE");
-		request.setCredentials(credential);
-		request.setQuery("1 Microsoft Way, Redmond, WA");
-		
-		GeocodeOptions geocodeOptions = GEOCODE_FACTORY.createGeocodeOptions();
-
-		ArrayOfFilterBase array = GEOCODE_FACTORY.createArrayOfFilterBase();
-		ConfidenceFilter filter = GEOCODE_FACTORY.createConfidenceFilter();
-		filter.setMinimumConfidence(Confidence.HIGH);
-		array.getFilterBase().add(filter);
-		geocodeOptions.setFilters(array);
-		request.setOptions(geocodeOptions);
-		
-		return request;
+		GeocodeRequestBuilder builder = client.newGeocodeRequestBuilder();
+		builder.withCredentials("AgBXisHgZAEfpDnT95skGJiYu_Oh9XgeAi7O0UJfhg_GdEYB2yeeETJ8ayQ-3kNE", null);
+		builder.withQuery("1 Microsoft Way, Redmond, WA");
+		builder.withOptionsFilter(Confidence.HIGH);
+		return builder.getResult();
 	}
 }
